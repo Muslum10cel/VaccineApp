356c07adcc85f287006c90eb4f3c55a225e9dff6
muslumoncel <muslumoncel@192.168.1.105>
2015-12-09 00:51:04 +0200

    Vaccine

--- src/java/com/muslumyusuf/DbFunctions.java
+++ src/java/com/muslumyusuf/DbFunctions.java
@@ -11,5 +11,7 @@ package com.muslumyusuf;
  */
 public class DbFunctions {
 
-	public static final String CHECK_USER_FUNCTION = "select db_9c4db8_dprtmnt.checkUserFunc(?);";
+	public static final String CHECK_USER_FUNCTION = "select db_9c4db8_dprtmnt.checkUserFunc(?)";
+	public static final String LOG_IN = "select db_9c4db8_dprtmnt.log_in(?,?)";
+	public static final String REGISTER = "select db_9c4db8_dprtmnt.Register(?,?,?)";
 }
